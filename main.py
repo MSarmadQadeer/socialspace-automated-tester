e8fcab58088a8957e8b81d7a9f69da5c3a755d08
M Sarmad Qadeer <MSarmadQadeer@gmail.com>
2023-01-10 18:32:17 +0500

    updated python tester

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,10 +10,10 @@ import time
 
 
 def chrome_browser(url):
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     options.add_argument('headless')
-    browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     browser.set_window_size(1080, 720)
     browser.get(url)
     return browser
@@ -21,14 +23,16 @@ def login(browser, username, password):
     try:
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".login-form input[name='email']")))
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".login-form input[name='password']")))
-        browser.find_element_by_css_selector(".login-form input[name='email']").send_keys(username)
-        browser.find_element_by_css_selector(".login-form input[name='password']").send_keys(password)
+        browser.find_element(By.CSS_SELECTOR,".login-form input[name='email']").send_keys(username)
+        browser.find_element(By.CSS_SELECTOR,".login-form input[name='password']").send_keys(password)
 
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".login-form .login-btn")))
-        browser.find_element_by_css_selector(".login-form .login-btn").click()
+        browser.find_element(By.CSS_SELECTOR,".login-form .login-btn").click()
+
+        time.sleep(10) # wait for the page to load after login
 
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".more-icon-btn")))
-        browser.find_element_by_css_selector(".more-icon-btn").click()
+        browser.find_element(By.CSS_SELECTOR,".more-icon-btn").click()
 
         print("Log In Success")
         return True
@@ -42,13 +46,13 @@ def test_create_post(browser, post_caption):
 
     try:    
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".create-post-icon")))
-        browser.find_element_by_css_selector(".create-post-icon").click()
+        browser.find_element(By.CSS_SELECTOR,".create-post-icon").click()
 
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".post-creation-panel .post-text-caption")))
-        browser.find_element_by_css_selector(".post-creation-panel .post-text-caption").send_keys(post_caption)
+        browser.find_element(By.CSS_SELECTOR,".post-creation-panel .post-text-caption").send_keys(post_caption)
 
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".post-creation-panel .post-btn")))
-        browser.find_element_by_css_selector(".post-creation-panel .post-btn").click()
+        browser.find_element(By.CSS_SELECTOR,".post-creation-panel .post-btn").click()
 
         should_run = True
     except:
@@ -57,12 +61,12 @@ def test_create_post(browser, post_caption):
 
     if should_run:
         try:
-            time.sleep(3) # wait for the page to load after post creation
+            time.sleep(10) # wait for the page to load after post creation
             WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".post")))
-            posts = browser.find_elements_by_css_selector(".post")
+            posts = browser.find_elements(By.CSS_SELECTOR, ".post")
             for post in posts:
                 WebDriverWait(post, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".caption")))
-                caption = post.find_element_by_css_selector(".caption")
+                caption = post.find_element(By.CSS_SELECTOR,".caption")
                 if caption.text == post_caption:
                     post_id = post.get_attribute("data-id")
                     print("Post Creation Success")
@@ -78,17 +82,17 @@ def test_create_post(browser, post_caption):
 def test_like_post(browser,post_id):
     try:    
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".post")))
-        posts = browser.find_elements_by_css_selector(".post")
+        posts = browser.find_elements(By.CSS_SELECTOR, ".post")
         for post in posts:
             if post.get_attribute("data-id") == post_id:
                 WebDriverWait(post, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".likes-count")))
-                likes_count_before_like = post.find_element_by_css_selector(".likes-count").text
+                likes_count_before_like = post.find_element(By.CSS_SELECTOR,".likes-count").text
                 likes_count_before_like = int(likes_count_before_like)
-                post.find_element_by_css_selector(".unchecked-heart").click()
+                post.find_element(By.CSS_SELECTOR,".unchecked-heart").click()
 
-                time.sleep(1) # wait to see the post getting liked
+                time.sleep(3) # wait to see the post getting liked
 
-                likes_count_after_like = post.find_element_by_css_selector(".likes-count").text
+                likes_count_after_like = post.find_element(By.CSS_SELECTOR,".likes-count").text
                 likes_count_after_like = int(likes_count_after_like)
 
                 if likes_count_after_like > likes_count_before_like:
@@ -105,17 +109,17 @@ def test_like_post(browser,post_id):
 def test_unlike_post(browser,post_id):
     try: 
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".post")))
-        posts = browser.find_elements_by_css_selector(".post")
+        posts = browser.find_elements(By.CSS_SELECTOR, ".post")
         for post in posts:
             if post.get_attribute("data-id") == post_id:
                 WebDriverWait(post, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".likes-count")))
-                likes_count_before_like = post.find_element_by_css_selector(".likes-count").text
+                likes_count_before_like = post.find_element(By.CSS_SELECTOR,".likes-count").text
                 likes_count_before_like = int(likes_count_before_like)
-                post.find_element_by_css_selector(".checked-heart").click()
+                post.find_element(By.CSS_SELECTOR,".checked-heart").click()
 
-                time.sleep(1) # wait to see the post getting unliked
+                time.sleep(3) # wait to see the post getting unliked
 
-                likes_count_after_like = post.find_element_by_css_selector(".likes-count").text
+                likes_count_after_like = post.find_element(By.CSS_SELECTOR,".likes-count").text
                 likes_count_after_like = int(likes_count_after_like)
 
                 if likes_count_after_like < likes_count_before_like:
@@ -134,23 +138,23 @@ def test_update_post(browser, post_id, post_caption):
 
     try: 
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".post")))
-        posts = browser.find_elements_by_css_selector(".post")
+        posts = browser.find_elements(By.CSS_SELECTOR, ".post")
         for post in posts:
             if post.get_attribute("data-id") == post_id:
                 WebDriverWait(post, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".three-dot-icon-btn")))
-                post.find_element_by_css_selector(".three-dot-icon-btn").click()
+                post.find_element(By.CSS_SELECTOR,".three-dot-icon-btn").click()
 
                 WebDriverWait(post, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".edit-post-tab a")))
-                post.find_element_by_css_selector(".edit-post-tab a").click()
+                post.find_element(By.CSS_SELECTOR,".edit-post-tab a").click()
 
-                time.sleep(2) # wait for the data to load in updation panel
+                time.sleep(6) # wait for the data to load in updation panel
 
                 WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".post-updation-panel .post-text-caption")))
-                browser.find_element_by_css_selector(".post-updation-panel .post-text-caption").clear()
-                browser.find_element_by_css_selector(".post-updation-panel .post-text-caption").send_keys(post_caption)
+                browser.find_element(By.CSS_SELECTOR,".post-updation-panel .post-text-caption").clear()
+                browser.find_element(By.CSS_SELECTOR,".post-updation-panel .post-text-caption").send_keys(post_caption)
 
                 WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".post-updation-panel .update-btn")))
-                browser.find_element_by_css_selector(".post-updation-panel .update-btn").click()
+                browser.find_element(By.CSS_SELECTOR,".post-updation-panel .update-btn").click()
                 break
         should_run = True
     except:
@@ -159,12 +163,12 @@ def test_update_post(browser, post_id, post_caption):
 
     if should_run:
         try:
-            time.sleep(3) # wait for the page to load after post updation
+            time.sleep(10) # wait for the page to load after post updation
             WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".post")))
-            posts = browser.find_elements_by_css_selector(".post")
+            posts = browser.find_elements(By.CSS_SELECTOR, ".post")
             for post in posts:
                 WebDriverWait(post, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".caption")))
-                caption = post.find_element_by_css_selector(".caption")
+                caption = post.find_element(By.CSS_SELECTOR,".caption")
                 if caption.text == post_caption:
                     print("Post Updation Success")
                     return True
@@ -181,11 +185,11 @@ def test_delete_post(browser,post_id):
 
     try:    
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".post")))
-        posts = browser.find_elements_by_css_selector(".post")
+        posts = browser.find_elements(By.CSS_SELECTOR, ".post")
         for post in posts:
             if post.get_attribute("data-id") == post_id:
-                post.find_element_by_css_selector(".three-dot-icon-btn").click()
-                post.find_element_by_css_selector(".delete-post-tab a").click()
+                post.find_element(By.CSS_SELECTOR,".three-dot-icon-btn").click()
+                post.find_element(By.CSS_SELECTOR,".delete-post-tab a").click()
                 break   
         should_run = True     
     except:
@@ -194,9 +198,9 @@ def test_delete_post(browser,post_id):
 
     if should_run:
         try:
-            time.sleep(3) # wait for the page to load after post deletion
+            time.sleep(10) # wait for the page to load after post deletion
             WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".post")))
-            posts = browser.find_elements_by_css_selector(".post")
+            posts = browser.find_elements(By.CSS_SELECTOR, ".post")
             for post in posts:
                 if post.get_attribute("data-id") == post_id:
                     print("Post Deletion Failed")
@@ -275,7 +279,9 @@ def main():
         browser.close()
     except:
         print("Browser Failed")
-    
+        file.write("Browser Failed")
+        file.write("\n")
+
     file.close()
 
 
